b272f94c1b88eb48fed80a5d76ae8a9181c9afc4
wdwbftc <wdwbftc.bot@gmail.com>
2022-01-16 10:49:41 +0100

    Added PRAW exception handling.

--- looper.py
+++ looper.py
@@ -1,6 +1,8 @@
+import logging
 import time
 
 import praw
+import prawcore.exceptions
 
 from worker import Worker
 
@@ -8,6 +10,7 @@ from worker import Worker
 class Looper:
 
     def __init__(self, config: dict, reddit: praw.Reddit):
+        self.logger = logging.getLogger(__name__)
         self.config = config
         self.reddit = reddit
 
@@ -15,13 +18,18 @@ class Looper:
         worker = Worker(self.config, self.reddit)
         loop_seconds = self.config['settings']['loop_minutes'] * 60
         if loop_seconds == 0:
-            worker.process()
+            self.try_process(worker)
             return
 
         while True:
             last_time = time.time()
-            worker.process()
+            self.try_process(worker)
             seconds_passed = time.time() - last_time
             if seconds_passed < loop_seconds:
                 time.sleep(loop_seconds - seconds_passed)
 
+    def try_process(self, worker):
+        try:
+            worker.process()
+        except prawcore.PrawcoreException as e:
+            self.logger.error(f'PRAW exception occurred: {e}')
